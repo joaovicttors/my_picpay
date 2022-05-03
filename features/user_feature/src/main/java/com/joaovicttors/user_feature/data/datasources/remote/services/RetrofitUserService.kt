0fcdbe975c22ee4a0b9615f0d2323c76cc791125
joaovicttors <joaovicttorrs@gmail.com>
2022-05-02 21:04:31 -0300

    add retrofit annotations

--- features/user_feature/src/main/java/com/joaovicttors/user_feature/data/datasources/remote/services/RetrofitUserService.kt
+++ features/user_feature/src/main/java/com/joaovicttors/user_feature/data/datasources/remote/services/RetrofitUserService.kt
@@ -1,8 +1,14 @@
 package com.joaovicttors.user_feature.data.datasources.remote.services
 
 import com.joaovicttors.user_feature.data.models.UserResponse
+import retrofit2.http.GET
 
 interface RetrofitUserService {
 
+    @GET(USER_ENDPOINT)
     suspend fun getUserList(): List<UserResponse>
+
+    companion object {
+        const val USER_ENDPOINT: String = "users"
+    }
 }
--- features/user_feature/src/main/java/com/joaovicttors/user_feature/data/models/UserResponse.kt
+++ features/user_feature/src/main/java/com/joaovicttors/user_feature/data/models/UserResponse.kt
@@ -1,8 +1,18 @@
 package com.joaovicttors.user_feature.data.models
 
+import com.google.gson.annotations.SerializedName
+
 data class UserResponse(
-    val image: String?,
-    val name: String?,
-    val id: Int?,
-    val username: String?
-)
+    @SerializedName(IMAGE) val image: String?,
+    @SerializedName(NAME) val name: String?,
+    @SerializedName(ID) val id: Int?,
+    @SerializedName(USERNAME) val username: String?
+) {
+
+    private companion object {
+        private const val IMAGE: String = "img"
+        private const val NAME: String = "name"
+        private const val ID: String = "id"
+        private const val USERNAME: String = "username"
+    }
+}
